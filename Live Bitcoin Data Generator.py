32121f14447d6ab51e970a4f106c1d74e703ae05
Sourin Das <99353367+SourinDas2000@users.noreply.github.com>
2022-03-08 13:22:56 +0530

    Add files via upload

---
+++ Live Bitcoin Data Generator.py
@@ -0,0 +1,69 @@
+
+# This program creates columns of live Bitcoin prices every minute in a csv file. The program also outputs the data in order to see the containt of the it in real time.
+
+import csv
+import time
+import requests
+import fontstyle
+
+from datetime import datetime
+
+
+# Getting data from Crypto Compare :
+
+url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'
+
+def get_price(coin, currency):
+    
+    try:
+        
+        response = requests.get(url.format(coin, currency)).json()
+        
+        return response
+        
+    except requests.ConnectionError:
+        
+        error_statement = fontstyle.apply(" Problem Getting To The Link. Make sure you are connected to the internet.", "yellow")
+        
+        print(fontstyle.apply('Error:', 'bold/red/underline') + error_statement)
+
+
+# Using the data to creat a CSV file :
+
+fieldnames = ["time", "current price(₹)"]
+
+
+with open('Live Bitcoin Price Today.csv', 'w') as csv_file:
+    
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    
+    csv_writer.writeheader()
+       
+     
+while True:
+    
+    today = datetime.now()
+    date_time = today.strftime('%H:%M')
+    current_price = get_price('BTC', 'INR')
+  
+    
+    with open('Live Bitcoin Price Today.csv', 'a') as csv_file:
+        
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        
+        try:
+         info = {
+         "time": date_time,
+         "current price(₹)": current_price['INR']
+         }   
+            
+         csv_writer.writerow(info)
+         print('At', date_time,'Price of 1 Bitcoin is Rs.', current_price['INR'])
+
+         time.sleep(60)
+         
+        except (TypeError, NameError): 
+         break
+        
+
+''' Created By Sourin Das '''
